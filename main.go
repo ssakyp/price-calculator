95ae83b354038f4bd2296d5ff1644217c11fb8b1
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-30 08:46:23 +0500

    Update main.go

--- main.go
+++ main.go
@@ -10,12 +10,14 @@ import (
 func main() {
 
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
+	doneChans := make([]chan bool, len(taxRates))
 
-	for _, taxRate := range taxRates {
+	for i, taxRate := range taxRates {
+		doneChans[i] = make(chan bool)
 		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
 		//cmdm := cmdmanager.New()
 		priceJob := prices.NewTaxIncludedPriceJob(*fm, taxRate)
-		go priceJob.Process()
+		go priceJob.Process(doneChans[i])
 
 		// if err != nil {
 		// 	fmt.Println("Could not process job")
