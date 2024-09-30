dec6f2ccaca3bee68dda531dea3d1835042d6949
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-30 08:41:52 +0500

    Update filemanager.go

--- filemanager/filemanager.go
+++ filemanager/filemanager.go
@@ -5,6 +5,7 @@ import (
 	"encoding/json"
 	"errors"
 	"os"
+	"time"
 )
 
 type FileManager struct {
@@ -45,6 +46,8 @@ func (fm FileManager) WriteResult( data interface{}) error {
 		return errors.New("Failed to create a file.")
 	}
 
+
+	time.Sleep(3*time.Second)
 	encoder := json.NewEncoder(file)
 	err = encoder.Encode(data)
 
