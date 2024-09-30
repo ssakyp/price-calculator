0890879bae6cee40be3d5dbbf60da7aa95e25ff5
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-30 09:00:31 +0500

    Update main.go

--- main.go
+++ main.go
@@ -11,13 +11,15 @@ func main() {
 
 	taxRates := []float64{0, 0.07, 0.1, 0.15}
 	doneChans := make([]chan bool, len(taxRates))
+	errorChans := make([]chan error, len(taxRates))
 
 	for i, taxRate := range taxRates {
 		doneChans[i] = make(chan bool)
+		errorChans[i] = make(chan error)
 		fm := filemanager.New("prices.txt", fmt.Sprintf("result_%.0f.json", taxRate*100))
 		//cmdm := cmdmanager.New()
 		priceJob := prices.NewTaxIncludedPriceJob(*fm, taxRate)
-		go priceJob.Process(doneChans[i])
+		go priceJob.Process(doneChans[i], errorChans[i])
 
 		// if err != nil {
 		// 	fmt.Println("Could not process job")
@@ -25,8 +27,23 @@ func main() {
 		// }
 	}
 
-	for _, doneChan := range doneChans {
-		<- doneChan
+	for i, _ := range taxRates {
+		// select allows us to wait for one channel, it will move on, it waits only for the one
+		// which is the fastest
+		select {
+		case err := <- errorChans[i]:
+			if err != nil {
+				fmt.Println(err)
+			}	
+		case <-doneChans[i]:
+			fmt.Println("Done!")
+		}
 	}
+
+
+	
+	// for _, doneChan := range doneChans {
+	// 	<- doneChan
+	// }
 
 }
