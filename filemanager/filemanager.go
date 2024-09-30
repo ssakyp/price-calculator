3ca10d82f23abec91f8e2aea61a525d09d44a76d
ssakyp <103821526+ssakyp@users.noreply.github.com>
2024-09-30 09:04:36 +0500

    Update filemanager.go

--- filemanager/filemanager.go
+++ filemanager/filemanager.go
@@ -20,6 +20,8 @@ func (fm FileManager) ReadLines() ([]string, error) {
 		return nil, errors.New("Failed to open file.")
 	}
 
+	defer file.Close()
+
 	scanner := bufio.NewScanner(file)
 
 	var lines []string
@@ -31,11 +33,11 @@ func (fm FileManager) ReadLines() ([]string, error) {
 	// this can be used to identify the error
 	scanner.Err()
 	if err != nil {
-		file.Close()
+		//file.Close()
 		return nil, errors.New("Failed to read file!")
 	}
 
-	file.Close()
+	//file.Close()
 	return lines, nil
 }
 
@@ -46,17 +48,18 @@ func (fm FileManager) WriteResult( data interface{}) error {
 		return errors.New("Failed to create a file.")
 	}
 
+	defer file.Close()
 
 	time.Sleep(3*time.Second)
 	encoder := json.NewEncoder(file)
 	err = encoder.Encode(data)
 
 	if err != nil {
-		file.Close()
+		//file.Close()
 		return errors.New("Failed to convert data to JSON.")
 	}
 
-	file.Close()
+	//file.Close()
 	return nil
 }
 
